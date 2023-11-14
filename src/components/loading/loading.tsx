6876c5f97a32937208437587426ca56e3d9c3d60
GoldBoy1001 <Sergiorabota133@gmail.com>
2023-11-14 13:53:37 +0300

    add files

---
+++ src/components/loading/loading.tsx
@@ -0,0 +1,14 @@
+export default function Loading() {
+  return (
+    <section className="loading">
+      <div className="loading__body">
+        <div className="loading__animated">
+          <p className="loading__animated-text">идет поиск</p>
+          <div className="loading__animated-image">
+            <img src="" alt="Train" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
